edc976c977115e204e91a1531a5efef4af988772
Éli Marshal <elimarshal@gmail.com>
2015-03-31 08:27:31 -0600

    Migrate from WPF to formless Windows Forms

--- LockShowWindowContents/Helpers/NativeMethods.cs
+++ LockShowWindowContents/Helpers/NativeMethods.cs
@@ -4,6 +4,7 @@
 
 namespace LockShowWindowContents.Helpers
 {
+    using System.Diagnostics.CodeAnalysis;
     using System.Runtime.InteropServices;
 
     /// <summary>
@@ -31,9 +32,29 @@ namespace LockShowWindowContents.Helpers
         /// </summary>
         public const short SPI_GETDRAGFULLWINDOWS = 0x26;
 
+        /// <summary>
+        /// SystemParametersInfo call.
+        /// </summary>
+        /// <param name="uAction">System parameter to query or set.</param>
+        /// <param name="uParam">Depends on system parameter.</param>
+        /// <param name="lpvParam">Depends on system parameter.</param>
+        /// <param name="fuWinIni">WIN.INI update flag.</param>
+        /// <returns>True if successful</returns>
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1625:ElementDocumentationMustNotBeCopiedAndPasted",
+            Justification = "Standard documentation.")]
         [DllImport("user32.dll", SetLastError = true)]
         public static extern bool SystemParametersInfo(int uAction, int uParam, ref bool lpvParam, int fuWinIni);
 
+        /// <summary>
+        /// SystemParametersInfo call.
+        /// </summary>
+        /// <param name="uAction">System parameter to query or set.</param>
+        /// <param name="uParam">Depends on system parameter.</param>
+        /// <param name="lpvParam">Depends on system parameter.</param>
+        /// <param name="fuWinIni">WIN.INI update flag.</param>
+        /// <returns>True if successful</returns>
+        [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1625:ElementDocumentationMustNotBeCopiedAndPasted",
+            Justification = "Standard documentation.")]
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
         public static extern bool SystemParametersInfo(int uAction, bool uParam, string lpvParam, int fuWinIni);
     }
---
+++ LockShowWindowContents/Program.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="Program.cs" company="Éli Marshal">
+// Copyright (c) Éli Marshal. All rights reserved.
+// </copyright>
+
+namespace LockShowWindowContents
+{
+    using LockShowWindowContents.Helpers;
+    using System;
+
+    /// <summary>
+    /// The main class to set "Show window contents while dragging" console-style.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            ShowWindowContents.SetShowWindowContents();
+        }
+    }
+}
